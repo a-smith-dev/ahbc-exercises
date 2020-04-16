b81d54fd3fb36b7a09bc05189a1f28f35da80883
andy <asmithdev91@gmail.com>
2020-04-15 21:38:22 -0400

    first commit for Exercise 7

---
+++ Exercise07/Exercise07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repeater = "";
+            var response = "";
+            do
+            {
+                Console.Write("Enter some text: ");
+                repeater = Console.ReadLine();
+                Console.WriteLine(repeater);
+                Console.Write("Would you like to continue (y/n)? ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+            } while (response == "Y");
+        }
+    }
+}
