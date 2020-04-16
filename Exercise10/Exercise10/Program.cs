59853e4412abd1836addb0645ab3b547cc2f0b25
andy <asmithdev91@gmail.com>
2020-04-15 22:05:09 -0400

    first commit for Exercise 10, just for funsies

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal minHeight = 54;
+            var height = 0.0m;
+            var difference = height;
+            var repeatResponse = "";
+
+            do
+            {
+
+                Console.Write("Enter your height in inches: ");
+                height = Convert.ToDecimal(Console.ReadLine());
+
+                if (height < minHeight)
+                {
+                    difference = minHeight - height;
+                    Console.WriteLine("Sorry, you cannot ride the Raptor. You need " + difference +
+                        " more inches.");
+                }
+                else
+                {
+                    Console.WriteLine("Great, you can ride the Raptor!");
+                }
+                Console.Write("Would you like to continue (y/n) ");
+                repeatResponse = Console.ReadLine();
+                repeatResponse = repeatResponse.ToUpper();
+            } while (repeatResponse == "Y");
+        }
+    }
+}
