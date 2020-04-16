1ac2af72ec4a3e9d1b6bd7ae995660036aea2003
andy <asmithdev91@gmail.com>
2020-04-15 21:23:21 -0400

    first commit for Exercise 6

---
+++ Exercise06/Exercise06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var response = "";
+            do
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Would you like to continue (y/n)? ");
+                response = Console.ReadLine();
+            } while (response == "y" || response == "Y");
+        }
+    }
+}
