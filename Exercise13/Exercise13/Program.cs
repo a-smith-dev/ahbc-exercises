854fef1febd7e1a918eed83ac4e4727104a1f893
andy <asmithdev91@gmail.com>
2020-04-16 20:37:43 -0400

    First commit for Exercise 13

---
+++ Exercise13/Exercise13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            string repeater;
+
+            do
+            {
+                Console.Write("Enter a number: ");
+                input = int.Parse(Console.ReadLine());
+                for (int i = input; i >= 0; i--)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+                Console.Write("Would you like to continue (y/n)? ");
+                repeater = Console.ReadLine().ToUpper();
+            } while (repeater == "Y");
+        }
+    }
+}
