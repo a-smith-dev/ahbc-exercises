8e4cc3dec3c95283c87843832f37296beae928c7
andy <asmithdev91@gmail.com>
2020-04-19 21:04:42 -0400

    First commit for Exercise 23. Only one variable used.

---
+++ Exercise23/Exercise23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            do
+            {
+                Console.Write("Enter some text: ");
+                response = Console.ReadLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    if (response.Length - 1 < 10)
+                    {
+                        Console.Write(response);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(response[i]);
+                    }
+                }
+                Console.Write("\nWould you like to continue (y/n)? ");
+                response = Console.ReadLine().ToUpper();
+            } while (response == "Y");
+        }
+    }
+}
