4c07086b9993e97ea1d281bffda6b666671ba43b
andy <asmithdev91@gmail.com>
2020-04-20 21:46:57 -0400

    First commit for Exercise 17. Asks the user to set the height.

---
+++ Exercise17/Exercise17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a height for the triangle: ");
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine("Not a number.");
+            }
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
