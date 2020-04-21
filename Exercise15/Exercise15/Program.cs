29fd5c31054481619881c25f030f00f14998e139
andy <asmithdev91@gmail.com>
2020-04-20 21:28:27 -0400

    First commit for Exercise 15. Validated whether the user responds with 'y' or 'n' if they want to continue.

---
+++ Exercise15/Exercise15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            bool isNumber;
+            string repeater;
+            bool isRepeater;
+            do
+            {
+                do
+                {
+                    Console.Write("Enter a number: ");
+                    isNumber = double.TryParse(Console.ReadLine(), out number);
+                } while (!isNumber);
+                for (double i = 1; i <= number; i++)
+                {
+                    Console.Write($"{Math.Pow(i, 3)} ");
+                }
+                do
+                {
+                    Console.Write("\nWould you like to continue (y/n)? ");
+                    repeater = Console.ReadLine().ToUpper();
+                    isRepeater = Regex.IsMatch($"{repeater}", "[YN]");
+                } while (!isRepeater);
+            } while (repeater == "Y");
+        }
+    }
+}
