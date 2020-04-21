0ccd6974b925c9468af4025b9b6f5024765f117d
andy <asmithdev91@gmail.com>
2020-04-20 20:51:18 -0400

    First commit for Exercise 14. Used TryParse and Math.Pow for the first time.

---
+++ Exercise14/Exercise14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            bool isNumber;
+            string repeater;
+            do
+            {
+                do
+                {
+                    Console.Write("Enter a number: ");
+                    isNumber = double.TryParse(Console.ReadLine(), out number);
+                } while (!isNumber);
+                for (double i = 1; i <= number; i++)
+                {
+                    Console.Write($"{Math.Pow(i, 2)} ");
+                }
+                Console.Write("\nWould you like to continue (y/n)? ");
+                repeater = Console.ReadLine().ToUpper();
+            } while (repeater == "Y");
+        }
+    }
+}
