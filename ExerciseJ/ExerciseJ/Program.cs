d755c7ae5e3a0667d42c60199b02701ed12a20aa
andy <asmithdev91@gmail.com>
2020-04-22 22:04:53 -0400

    Jason's homebrew exercise on recursion. (ExerciseJ)

---
+++ ExerciseJ/ExerciseJ/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 1, 2, 3, 4, 5 };
+            int number;
+            do
+            {
+              Console.Write("Enter a number: ");
+            } while (!int.TryParse(Console.ReadLine(), out number));
+            Console.WriteLine(BinarySearch(array, number));
+        }
+
+        private static bool BinarySearch(int[] array, int number)
+        {
+            var middle = array.Length / 2;
+            if (array[middle] == number)
+            {
+                return true;
+            }
+            else if (array[middle] > number)
+            {
+                return BinarySearch(Right(array), number);
+            }
+            else if (array[middle] < number) // functions the same as "else"
+            {
+                return BinarySearch(Left(array), number);
+            }
+            return false;
+        }
+
+        private static int[] Left(int[] array)
+        {
+            return array.Take(array.Length / 2).ToArray();
+        }
+
+        private static int[] Right(int[] array)
+        {
+            return array.Skip(array.Length / 2).ToArray();
+        }
+
+    }
+}
