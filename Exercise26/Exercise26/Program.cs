57533d07aa1b9a297a0cbd206b1c300f628af20f
andy <asmithdev91@gmail.com>
2020-05-04 18:27:25 -0400

    First commit for Exercises 26 & 27. Checking for vowels and consonants in string inputs.

---
+++ Exercise26/Exercise26/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Enter some text: ");
+                var letters = Console.ReadLine().ToCharArray();
+                Console.WriteLine($"There were {NumOfVowels(letters)} vowels.");
+                Console.Write("Would you like to continue? (y/n) ");
+            } while (ValidateYesNo(Console.ReadLine()) == "y");
+        }
+
+        private static int NumOfVowels(char[] response)
+        {
+            var num = 0;
+            for (int i = 0; i < response.Length; i++)
+            {
+                if (Regex.IsMatch($"{response[i]}", "[aeiou]"))
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        private static string ValidateYesNo(string response)
+        {
+            response = response.ToLower();
+            while (!Regex.IsMatch($"{response}", "^[yn]$"))
+            {
+                Console.Write("Please enter y or n: ");
+                response = Console.ReadLine().ToLower();
+            }
+            return response;
+        }
+    }
+}
---
+++ Exercise27/Exercise27/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Enter some text: ");
+                var letters = Console.ReadLine().ToCharArray();
+                Console.WriteLine($"There were {NumOfConsonants(letters)} consonants.");
+                Console.Write("Would you like to continue? (y/n)");
+            } while (ValidateYesNo(Console.ReadLine()) == "y");
+        }
+
+        private static int NumOfConsonants(char[] letters)
+        {
+            var counter = 0;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (Regex.IsMatch($"{letters[i]}", "[bcdfghjklmnpqrstvwxyz]"))
+                    counter++;
+            }
+            return counter;
+        }
+
+        private static string ValidateYesNo(string response)
+        {
+            response = response.ToLower();
+            while (!Regex.IsMatch($"{response}", "^[yn]$"))
+            {
+                Console.Write("Please enter y or n: ");
+                response = Console.ReadLine().ToLower();
+            }
+            return response;
+        }
+    }
+}
